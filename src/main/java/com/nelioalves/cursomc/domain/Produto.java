cfef624cebc24b88fe5198fdc060a380dc4f090f
Pedro Henrique Iwamoto <phiwamoto@hotmail.com>
2018-03-12 22:19:49 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/nelioalves/cursomc/domain/Produto.java
+++ src/main/java/com/nelioalves/cursomc/domain/Produto.java
@@ -37,6 +37,7 @@ public class Produto implements Serializable {
 	)
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	@com.fasterxml.jackson.annotation.JsonIgnore
 	@OneToMany(mappedBy="id.produto")	
 	private Set<ItemPedido> itens = new HashSet<>();	
 	
@@ -51,6 +52,7 @@ public class Produto implements Serializable {
 		this.preco = preco;
 	}
 	
+	@com.fasterxml.jackson.annotation.JsonIgnore
 	public List<Pedido> getPedidos() {
 		List<Pedido> lista = new ArrayList<>();
 		for (ItemPedido x : itens) {
